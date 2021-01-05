39f5196bb205e71d426aa696f54cb75933c5f805
Denis <denis@efojs.com>
2021-01-06 02:02:55 +0300

    Fix axios vulnerability and fix deprecation in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           path.resolve(__dirname, "bower_components"),
         ],
         loader: "babel-loader",
-        query: {
+        options: {
           presets: [
             "@babel/preset-env",
             "@babel/preset-react"
